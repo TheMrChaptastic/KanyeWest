0c5fa9a7c9f13653a5e06b050bd4b54892a229dd
TheMrChaptastic <chappy@themrchaptastic.com>
2022-02-08 16:47:44 -0600

    Add project files.

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            for (int i = 0; i < 5; i++)
+            {
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+                Console.WriteLine("Kanye says \"" + kanyeQuote + "\"");
+                Console.WriteLine("Ron responds with " + ronQuote);
+                Console.WriteLine();
+            }
+        }
+    }
+}
